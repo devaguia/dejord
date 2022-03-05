3cc54bc85af70386103122f57a0cb79430d339bf
Matheus Aguiar <matheus.aguiar@apiki.com>
2022-03-05 13:18:04 -0300

    feat: adicionando projeto react

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+import Header from './components/Header';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import New from './pages/New';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/new' element={<New/>}></Route>
+        <Route path='/edit' element={<Edit/>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+      <GlobalStyles/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
